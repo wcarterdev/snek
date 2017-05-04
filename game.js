59b66b8e885e0d48a729a9f66a52aacc92e9b1ef
Wes <paperziggurat@gmail.com>
2017-05-04 16:19:50 -0500

    game loop, fps

--- game.js
+++ game.js
@@ -1,18 +1,25 @@
 var canvas = document.querySelector('canvas');
-canvas.width = 800;
-canvas.height = 800;
 var ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 500;
 
-var dx = 5;
-var dy = 5;
+var fps = 10;
+var block_dimension = 10;
+
 
 var snek = {
   head: {
-    x: 0,
-    y: 0
+    x: 250,
+    y: 250
   },
-  body: []
+  body: [],
+  dx: 0,
+  dy: 0
 };
+
+snek.run = function() {
+  update();
+}
 
 //ctx.beginPath();
 //ctx.lineWidth = "1";
@@ -26,31 +33,40 @@ window.addEventListener('keydown', keyDown, false);
 function keyDown(e) {
   if (e.keyCode === 37) {
     if (snek.head.x > 0) {
-      //update();
+      snek.dx = -10;
+      snek.dy = 0;
     }
   }
   if (e.keyCode === 38) {
     if (snek.head.y > 0) {;
-      //update();
+      snek.dx = 0;
+      snek.dy = -10;
     }
   }
   if (e.keyCode === 39) {
-    if (snek.head.x <= 780) {
-      console.log(snek.head.x);
-      //update();
+    if (snek.head.x < canvas.width - block_dimension) {
+      snek.dx = 10;
+      snek.dy = 0;
     }
   }
   if (e.keyCode === 40) {
-    if (snek.head.y <= 780) {
-      //update();
+    if (snek.head.y < canvas.height - block_dimension) {
+      snek.dx = 0;
+      snek.dy = 10;
     }
   }
 }
 
+function test() {
+  console.log('test');
+}
+
+setInterval(snek.run, 1000 / fps);
+
 function init() {
   ctx.clearRect(0, 0, 800, 800);
   ctx.fillStyle = "red";
-  ctx.fillRect(snek.head.x, snek.head.y, 20, 20);
+  ctx.fillRect(snek.head.x, snek.head.y, 10, 10);
   //ctx.beginPath();
   //ctx.lineWidth = "1";
   //ctx.strokeStyle = "red";
@@ -58,20 +74,15 @@ function init() {
   ctx.stroke();
 }
 
-
-function tick() {
-  while (true) {
-    console.log('hey');
-    //setTimeout(update(), 2000);
-  }
-}
-
-
 function update() {
   //requestAnimationFrame(update);
+  var posx = snek.head.x + snek.dx;
+  var posy = snek.head.y + snek.dy;
+  snek.head.x += snek.dx;
+  snek.head.y += snek.dy;
   ctx.clearRect(0, 0, 800, 800);
-  ctx.fillRect(snek.head.x + dx, snek.head.y + dy, 20, 20);
+  ctx.fillStyle = "red";
+  ctx.fillRect(posx, posy, block_dimension, block_dimension);
 }
 
 init();
-tick();
