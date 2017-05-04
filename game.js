8c42189d6ca83283ecd94edb9ab910003b5cec88
Wes <wcarter.dev@gmail.com>
2017-05-04 16:49:57 -0500

    boundaries update + end logic

--- game.js
+++ game.js
@@ -3,13 +3,25 @@ var ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-var fps = 10;
+var fps = 30;
 var block_dimension = 10;
+
+var boundaries = {
+  left: 0,
+  up: 0,
+  right: canvas.width - block_dimension,
+  down: canvas.height - block_dimension
+};
+
+var food = {
+  x: 100,
+  y: 110
+}
 
 var snek = {
   head: {
-    x: 250,
-    y: 250
+    x: 300,
+    y: 300
   },
   body: [],
   dx: 0,
@@ -20,52 +32,58 @@ snek.run = function() {
   update();
 }
 
+function end() {
+  clearInterval(snek._intervalId);
+  init();
+}
+
 window.addEventListener('keydown', keyDown, false);
 
 function keyDown(e) {
   if (e.keyCode === 37) {
-    if (snek.head.x > 0) {
-      snek.dx = -10;
-      snek.dy = 0;
-    }
+    snek.dx = -10;
+    snek.dy = 0;
   }
   if (e.keyCode === 38) {
-    if (snek.head.y > 0) {;
-      snek.dx = 0;
-      snek.dy = -10;
-    }
+    snek.dx = 0;
+    snek.dy = -10;
   }
   if (e.keyCode === 39) {
-    if (snek.head.x < canvas.width - block_dimension) {
-      snek.dx = 10;
-      snek.dy = 0;
-    }
+    snek.dx = 10;
+    snek.dy = 0;
   }
   if (e.keyCode === 40) {
-    if (snek.head.y < canvas.height - block_dimension) {
-      snek.dx = 0;
-      snek.dy = 10;
-    }
+    snek.dx = 0;
+    snek.dy = 10;
   }
 }
 
 function init() {
-  ctx.clearRect(0, 0, 800, 800);
+  snek._intervalId = setInterval(snek.run, 1000 / fps);
+  console.log('called');
+  snek.head.x = 300,
+  snek.head.y = 300;
+  ctx.clearRect(0, 0, 600, 600);
   ctx.fillStyle = "red";
   ctx.fillRect(snek.head.x, snek.head.y, 10, 10);
   ctx.stroke();
 }
 
 function update() {
-  var posx = snek.head.x + snek.dx;
-  var posy = snek.head.y + snek.dy;
-  snek.head.x += snek.dx;
-  snek.head.y += snek.dy;
-  ctx.clearRect(0, 0, 800, 800);
-  ctx.fillStyle = "red";
-  ctx.fillRect(posx, posy, block_dimension, block_dimension);
-}
+  if (snek.head.x >= boundaries.left && snek.head.y >= boundaries.up
+    && snek.head.x <= boundaries.right && snek.head.y <= boundaries.down) {
 
-setInterval(snek.run, 1000 / fps);
+    var posx = snek.head.x + snek.dx;
+    var posy = snek.head.y + snek.dy;
+    snek.head.x += snek.dx;
+    snek.head.y += snek.dy;
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.fillStyle = "red";
+    ctx.fillRect(posx, posy, block_dimension, block_dimension);
+    
+  } else {
+    end();
+  }
+}
 
 init();
