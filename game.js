e46494fc7af8af45984eb2004a34a51d7274dc09
Wes <wcarter.dev@gmail.com>
2017-05-04 22:19:21 -0500

    abstracted some functions

--- game.js
+++ game.js
@@ -83,27 +83,36 @@ function init()
   ctx.stroke();
 }
 
+function drawSnek() 
+{ 
+  var posx = snek.head.x + snek.dx;
+  var posy = snek.head.y + snek.dy;
+  snek.head.x += snek.dx;
+  snek.head.y += snek.dy;
+  ctx.clearRect(0, 0, 600, 600);
+  ctx.fillStyle = "red";
+  ctx.fillRect(posx, posy, block_dimension, block_dimension); 
+}
+
+function drawFood()
+{
+  ctx.fillStyle = "white";
+  ctx.fillRect(food.x, food.y, block_dimension, block_dimension);  
+}
+
 function update() 
 {
   if (snek.head.x >= boundaries.left && snek.head.y >= boundaries.up
     && snek.head.x <= boundaries.right && snek.head.y <= boundaries.down) {
 
-    var posx = snek.head.x + snek.dx;
-    var posy = snek.head.y + snek.dy;
-    snek.head.x += snek.dx;
-    snek.head.y += snek.dy;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillStyle = "red";
-    ctx.fillRect(posx, posy, block_dimension, block_dimension);
-
-    if (snek.head.x === food.x && snek.head.y === food.y) {
+    if (snek.head.x === food.x && snek.head.y === food.y) {  
       food.x = Math.floor(Math.random() * (canvas.height / block_dimension)) * block_dimension;
       food.y = Math.floor(Math.random() * (canvas.width / block_dimension)) * block_dimension;
-      ctx.fillStyle = "white";
-      ctx.fillRect(food.x, food.y, block_dimension, block_dimension);
+      drawSnek();
+      drawFood();
     } else {
-      ctx.fillStyle = "white";
-      ctx.fillRect(food.x, food.y, block_dimension, block_dimension);
+      drawSnek();
+      drawFood();
     }
 
   } else {
